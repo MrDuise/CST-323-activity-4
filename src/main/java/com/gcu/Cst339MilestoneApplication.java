717491806e9e17551b2e613a8a9539d7bfa1bb7f
Michael Duisenberg <mrduise@gmail.com>
2023-03-05 12:04:12 -0700

    Update Cst339MilestoneApplication.java

--- src/main/java/com/gcu/Cst339MilestoneApplication.java
+++ src/main/java/com/gcu/Cst339MilestoneApplication.java
@@ -1,13 +1,19 @@
 package com.gcu;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.sun.tools.javac.Main;
 
 @SpringBootApplication
 public class Cst339MilestoneApplication {
 
 	public static void main(String[] args) {
+		 Logger logger = LoggerFactory.getLogger(Main.class);
 		SpringApplication.run(Cst339MilestoneApplication.class, args);
+		logger.info("MADE BY MICHAEL DUISENBERG");
 	}
 
 }
